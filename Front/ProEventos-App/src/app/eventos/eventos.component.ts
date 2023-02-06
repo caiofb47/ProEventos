629eefa72ae1688afaf7898c0b9685103e5942e4
caiofb47 <caiofb47@gmail.com>
2023-02-06 19:43:47 -0300

    Filtro

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,6 +10,28 @@ export class EventosComponent {
   constructor(private http: HttpClient) {}
 
   public eventos: any;
+  public eventosFiltrados: any;
+  _tableFilter: string = '';
+
+  public get tableFilter() {
+    return this._tableFilter;
+  }
+
+  public set tableFilter(value: string) {
+    this._tableFilter = value;
+    this.eventosFiltrados = this.tableFilter
+      ? this.FiltrarEventos(this.tableFilter)
+      : this.eventos;
+  }
+
+  FiltrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   ngOnInit(): void {
     this.getEventos();
@@ -17,8 +39,11 @@ export class EventosComponent {
 
   public getEventos(): void {
     this.http.get('http://localhost:5074/api/evento').subscribe(
-      response => this.eventos = response,
-      error => console.log(error),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+      (error) => console.log(error)
     );
   }
 }
